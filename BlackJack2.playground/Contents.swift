fa07e477b2ce8156ac9b71a41fba598fb99fd709
Maeda, Kazuya <kazuya.maeda@gmail.com>
2017-05-06 22:24:37 +0800

    first commit

---
+++ BlackJack2.playground/Contents.swift
@@ -0,0 +1,154 @@
+class Deal {
+    var playerNums: [Int] = []
+    var dealerNums: [Int] = []
+    
+    func getPlayerCounts() -> [Int] {
+        var playerCounts: [Int] = []
+        var playerCount = 0
+        var aceCount = 0
+        
+        for num in playerNums {
+            playerCount += num
+            
+            if (num == 1) { aceCount += 1 }
+        }
+        
+        playerCounts.append(playerCount)
+        
+        if (aceCount > 0) {
+            for i in 0..<aceCount {
+                playerCounts.append(playerCount + (10 * (i + 1)))
+            }
+        }
+        
+        return playerCounts
+    }
+    
+    func getDealerCount() -> Int {
+        var dealerCount = 0
+        
+        for num in dealerNums {
+            (num == 1) ? (dealerCount += 11) : (dealerCount += num)
+        }
+        
+        return dealerCount
+    }
+
+    func didPlayerWin() -> Bool {
+        for playerCount in getPlayerCounts() {
+            if (playerCount < 22 && (playerCount > getDealerCount() || getDealerCount() > 21)) {
+                return true
+            } else {
+                continue
+            }
+        }
+
+        return false
+    }
+}
+
+func capNums(nums: [Int]) -> [Int] {
+    var cappedNums: [Int] = []
+    
+    for num in nums {
+        (num > 10) ? cappedNums.append(10) : cappedNums.append(num)
+    }
+    print (cappedNums)
+    
+    return cappedNums
+}
+
+func shouldHit(deal: Deal, nums: [Int]) -> Bool {
+    deal.playerNums.append(nums[4])
+    
+//    print("\nChecking shouldHit")
+//    print("player nums: " + String(describing: deal.playerNums))
+//    print("dealer nums: " + String(describing: deal.dealerNums))
+
+    if (deal.didPlayerWin()) {
+        return true
+    } else {
+        return false
+    }
+}
+
+func game(count: Int, nums: [Int]) -> Int {
+    var winCount = count
+    let deal = Deal()
+    deal.playerNums = [nums[0], nums[2]]
+    deal.dealerNums = [nums[1], nums[3]]
+    
+    print("\nplayer nums: " + String(describing: deal.playerNums))
+    print("dealer nums: " + String(describing: deal.dealerNums))
+    
+    if (deal.didPlayerWin()) {
+        print("Win at first!")
+        winCount += 1
+
+        if (nums.count > 7) {
+            let restNums: [Int] = ([Int])(nums[4...(nums.count - 1)])
+            winCount = game(count: winCount, nums: restNums)
+        }
+    } else {
+        print("Loosing..")
+        
+        // Hit if player can win
+        if (shouldHit(deal: deal, nums: nums)) {
+            print("Should hit")
+            
+            // Hit and next
+            deal.dealerNums.append(nums[4])
+            
+            if (nums.count > 8) {
+                let restNums: [Int] = ([Int])(nums[5...(nums.count - 1)])
+                // Recur
+                winCount = game(count: winCount, nums: restNums)
+            }
+        } else if (nums.count > 7) {
+            let restNums: [Int] = ([Int])(nums[4...(nums.count - 1)])
+            winCount = game(count: winCount, nums: restNums)
+        }
+    }
+    
+    return winCount
+}
+
+func main() {
+    let orgNums = [10, 12, 4, 12, 9, 7, 7, 9, 7, 10, 5, 12, 4, 6, 8, 5, 13, 6, 1, 2, 5, 10, 8, 12, 6, 10, 13, 2, 1, 7, 9, 10]
+//let orgNums = [10, 12, 4, 12, 9, 7, 7, 9, 7, 10, 5, 12, 4, 6, 8, 5, 13, 6, 1, 2, 5, 10, 8, 12, 6, 10, 13, 2, 1, 7, 9, 10, 3, 13, 10, 11, 3, 10, 10, 11, 7, 6, 4, 11, 7, 9, 9, 10, 10, 3, 5, 10, 2, 11, 4, 6, 4, 2, 9, 12, 2, 8, 7, 12, 3, 1, 6, 9, 5, 10, 13, 11, 12, 10, 6, 1, 3, 10, 12, 9, 6, 9, 5, 3, 6, 12, 2, 12, 12, 6, 8, 5, 12, 8, 10, 12, 11, 9, 12, 5, 11, 9, 13, 4, 11, 7, 11, 8, 12, 1, 8, 6, 5, 7, 1, 10, 3, 4, 11, 10, 4, 12, 1, 1, 9, 1, 12, 1, 11, 5, 6, 10, 11, 6, 7, 5, 12, 6, 10, 7, 7, 10, 4, 13, 2, 3, 1, 1, 11, 13, 11, 10, 3, 2, 3, 8, 7, 8, 10, 9, 9, 12, 10, 11, 10, 11, 1, 5, 12, 2, 10, 7, 1, 8, 9, 3, 12, 10, 10, 5, 10, 1, 4, 6, 2, 4, 7, 1, 1, 5, 11, 11, 7, 9, 7, 6, 1, 8, 9, 2, 13, 5, 12, 5, 13, 12, 3, 2, 7, 11, 6, 12, 5, 12, 5, 11, 3, 9, 13, 7, 12, 2, 9, 3, 5, 2, 4, 9, 8, 2, 4, 12, 13, 8, 9, 12, 7, 3, 7, 10, 11, 5, 9, 1, 13, 7, 11, 5, 6, 5, 11, 3, 1, 13, 8, 9, 11, 7, 5, 6, 6, 12, 4, 8, 13, 5, 5, 10, 2, 8, 7, 2, 6, 4, 1, 13, 9, 11, 5, 7, 6, 4, 8, 6, 7, 8, 10, 6, 4, 7, 12, 2, 3, 5, 4, 5, 4, 5, 2, 7, 1, 8, 3, 7, 12, 3, 9, 3, 10, 8, 5, 8, 3, 3, 9, 13, 2, 12, 12, 9, 11, 2, 4, 2, 9, 7, 3, 8, 4, 9, 3, 6, 3, 7, 5, 13, 13, 7, 9, 8, 8, 12, 2, 1, 9, 2, 12, 4, 13, 3, 10, 11, 13, 2, 10, 5, 7, 10, 12, 1, 12, 6, 10, 2, 7, 5, 2, 9, 6, 13, 3, 12, 4, 7, 2, 13, 7, 13, 4, 10, 8, 1, 10, 13, 4, 10, 4, 10, 3, 2, 2, 13, 3, 8, 1, 2, 9, 8, 2, 1, 8, 5, 3, 3, 7, 12, 10, 13, 5, 9, 3, 4, 7, 1, 9, 1, 12, 10, 13, 6, 5, 10, 2, 12, 13, 4, 12, 7, 11, 12, 13, 6, 11, 3, 2, 7, 6, 4, 2, 9, 13, 12, 9, 6, 9, 3, 10, 13, 10, 12, 5, 12, 2, 12, 9, 6, 13, 7, 13, 5, 3, 12, 3, 9, 12, 7, 4, 11, 3, 3, 12, 8, 10, 8, 7, 8, 9, 5, 7, 8, 13, 3, 8, 7, 5, 5, 8, 9, 1, 13, 8, 1, 2, 1, 4, 5, 4, 12, 9, 5, 10, 1, 8, 3, 1, 13, 10, 13, 12, 6, 9, 10, 2, 6, 6, 4, 12, 6, 2, 1, 4, 11, 11, 7, 1, 2, 5, 1, 11, 9, 2, 10, 12, 11, 10, 12, 8, 2, 1, 5, 4, 2, 12, 9, 5, 7, 3, 6, 9, 3, 3, 10, 5, 9, 12, 4, 2, 8, 2, 3, 8, 11, 12, 10, 2, 9, 6, 4, 9, 5, 5, 6, 5, 8, 9, 5, 2, 5, 2, 6, 4, 12, 6, 8, 13, 4, 4, 13, 5, 9, 1, 7, 13, 4, 9, 8, 7, 10, 2, 3, 5, 6, 7, 5, 3, 7, 13, 7, 13, 1, 4, 2, 7, 8, 8, 3, 6, 8, 5, 5, 8, 6, 3, 6, 6, 9, 9, 5, 13, 10, 8, 2, 11, 4, 10, 13, 10, 3, 11, 8, 10, 6, 12, 7, 4, 7, 7, 8, 1, 9, 2, 3, 11, 7, 10, 11, 9, 12, 3, 8, 13, 5, 3, 11, 8, 6, 13, 1, 11, 10, 8, 7, 4, 11, 9, 6, 6, 10, 13, 9, 4, 5, 3, 6, 10, 13, 1, 6, 11, 11, 7, 6, 13, 10, 13, 9, 9, 4, 1, 3, 3, 10, 11, 13, 4, 2, 4, 4, 1, 12, 2, 2, 7, 12, 11, 12, 1, 1, 7, 1, 6, 2, 2, 6, 5, 4, 11, 9, 2, 13, 5, 11, 13, 4, 13, 6, 2, 9, 8, 13, 2, 2, 2, 11, 3, 12, 6, 5, 5, 6, 4, 7, 1, 13, 9, 8, 1, 10, 13, 10, 6, 8, 2, 10, 7, 3, 9, 1, 7, 8, 1, 3, 9, 3, 1, 8, 1, 8, 10, 6, 3, 9, 2, 2, 10, 7, 11, 9, 3, 5, 7, 2, 1, 9, 11, 7, 3, 10, 3, 6, 2, 13, 10, 8, 10, 8, 1, 7, 13, 1, 8, 13, 2, 13, 8, 8, 4, 5, 11, 3, 4, 6, 12, 6, 10, 9, 13, 3, 1, 2, 1, 1, 6, 1, 1, 8, 1, 8, 13, 13, 8, 2, 7, 10, 6, 13, 6, 4, 4, 5, 8, 9, 2, 6, 5, 7, 10, 8, 11, 3, 11, 8, 4, 3, 7, 9, 9, 2, 5, 13, 7, 6, 6, 3, 2, 13, 8, 9, 7, 5, 9, 4, 7, 13, 11, 3, 11, 13, 13, 2, 8, 12, 7, 7, 6, 7, 9, 1, 7, 12, 2, 8, 6, 11, 8, 9, 10, 13, 13, 5, 8, 12, 9, 11, 6, 12, 5, 5, 13, 3, 12, 6, 5, 7, 3, 9, 4, 9, 1, 1, 11, 11, 4, 5, 8, 9, 12, 1, 7, 13, 11, 4, 10, 8, 8, 9, 2, 1, 8, 12, 13, 4, 8, 3, 2, 5, 1, 1, 8, 13, 4, 1, 11, 12, 13, 7, 12, 11, 11, 10, 11, 6, 5, 11, 5, 12, 2, 5, 3, 13, 7, 7, 4, 6, 13, 8, 1, 6, 7, 4, 7, 2, 9, 5, 6, 1, 11, 12, 1, 8]
+    
+    print(orgNums)
+    
+    let nums: [Int] = capNums(nums: orgNums)
+    var winCount = 0
+    
+    ///////////
+//    let deal = Deal()
+//    deal.playerNums = [nums[0], nums[2]]
+//    deal.dealerNums = [nums[1], nums[3]]
+//
+//    if (deal.didPlayerWin()) {
+//        winCount += 1
+//        // Recur
+//    } else {
+//        // Hit if player can win
+//        if (shouldHit(deal: deal, nums: nums)) {
+//            // Hit and next
+//            deal.dealerNums.append(nums[4])
+//            winCount += 1
+//
+//            if (nums.count > 8) {
+//                // Recur
+//            }
+//        } else if (nums.count > 7) {
+//            // Recur
+//        }
+//
+//    }
+
+    winCount = game(count: winCount, nums: nums)
+    
+    print("winCount: " + String(winCount))
+}
+
+main()
